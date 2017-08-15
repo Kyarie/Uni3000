70735d1074f9702af8e0f1cd53133608449d8f09
Kyarie <dora-hu@hotmail.com>
2017-08-15 08:49:38 -0400

    User Header Fragment

--- app/src/main/java/com/uni3000/uni3000/view/home/HomeActivity.java
+++ app/src/main/java/com/uni3000/uni3000/view/home/HomeActivity.java
@@ -2,14 +2,22 @@ package com.uni3000.uni3000.view.home;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 
 import javax.inject.Inject;
 
 import dagger.android.AndroidInjection;
+import dagger.android.AndroidInjector;
+import dagger.android.DispatchingAndroidInjector;
+import dagger.android.support.HasSupportFragmentInjector;
 import com.uni3000.uni3000.R;
 import com.uni3000.uni3000.model.User;
+import com.uni3000.uni3000.view.user_header.UserHeaderFragment;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements HasSupportFragmentInjector {
+
+    @Inject
+    DispatchingAndroidInjector<Fragment> fragmentDispatchingAndroidInjector;
 
     @Inject
     User user;
@@ -18,7 +26,18 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         AndroidInjection.inject(this);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home2);
+        setContentView(R.layout.activity_home);
         user.num = 1;
+
+        if (savedInstanceState == null)
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.container, UserHeaderFragment.newInstance())
+                    .commitAllowingStateLoss();
+    }
+
+    @Override
+    public AndroidInjector<Fragment> supportFragmentInjector() {
+        return fragmentDispatchingAndroidInjector;
     }
 }
---
+++ app/src/main/java/com/uni3000/uni3000/view/user_header/UserHeaderModule.java
@@ -0,0 +1,11 @@
+package com.uni3000.uni3000.view.user_header;
+
+import dagger.Binds;
+import dagger.Module;
+import dagger.Provides;
+
+@Module
+public class UserHeaderModule {
+
+
+}
